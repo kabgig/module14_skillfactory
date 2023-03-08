f5ff872e1545277430b212a155b45d96e1706926
Ansar Ismagilov <ansar89kab@bk.ru>
2023-03-08 14:10:37 +0700

    bank example my version

---
+++ src/Client.java
@@ -0,0 +1,16 @@
+public class Client extends Thread {
+    Bank bank;
+
+    public Client(Bank bank) {
+        this.bank = bank;
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            bank.takeMoney();
+            System.out.println(Thread.currentThread().getName());
+            bank.repayMoney();
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,36 @@
-public class Main {
-    public static void main(String[] args) {
-        LoadingThread LT = new LoadingThread();
-        LT.start();
-        try {
-            LT.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+public class Bank {
+
+    private static int money = 10000;
+
+    static int getMoney() {
+        return money;
+    }
+
+    static synchronized void take(int money) {
+        Bank.money -= money;
+    }
+
+    static synchronized void repay(int money) {
+        Bank.money += money;
+    }
+
+    static class Client extends Thread{
+        @Override
+        public void run() {
+            while(true) {
+                take(1000);
+                repay(1000);
+            }
         }
-        System.out.print("Images are loaded");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new Client().start();
+        new Client().start();
+        new Client().start();
+        while(true) {
+            System.out.println(Bank.money);
+            Thread.sleep(1000);
+        }
     }
 }
