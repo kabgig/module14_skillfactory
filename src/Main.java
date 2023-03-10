ae436ef798ff4183203cf641a5cbbfd08758a210
Ansar Ismagilov <ansar89kab@bk.ru>
2023-03-10 13:50:59 +0700

    ex 1 additional DONE

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
+    public static void main(String[] args) {
 
+        ExecutorService e = Executors.newFixedThreadPool(2);
 
-    public static void main(String[] args) throws InterruptedException{
-
-        Pizzeria pizzeria = new Pizzeria();
-        pizzeria.order("Margarita");
-        Thread.sleep(500);
-        pizzeria.order("Pepperoni");
-        Thread.sleep(500);
-        pizzeria.order("Sicilian");
-        Thread.sleep(500);
-        pizzeria.order("Greek");
-
+        for (int i = 0; i < 5; i++) {
+            int b = i;
+            e.submit(new Thread() {
+                         @Override
+                         public void run() {
+                             try {
+                                 Thread.sleep(4000);
+                             } catch (InterruptedException ex) {
+                                 throw new RuntimeException(ex);
+                             }
+                             System.out.println(b + Thread.currentThread().getName() + " started.");
+                         }
+                     }
+            );
+        }
+            e.shutdown();
     }
-
 }
--- src/Pizzeria.java
+++ src/Pizzeria.java
@@ -1,36 +1,52 @@
-import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.TimeUnit;
 
-public class Pizzeria {
-    long start;
-
-    public Pizzeria() {
-        start = System.currentTimeMillis();
+class Pizzeria{
+    class Order{
+        String pizzaName;
+        long orderTime;
+        Order(String pizzaName, long orderTime){
+            this.pizzaName = pizzaName;
+            this.orderTime = orderTime;
+        }
     }
 
-    BlockingDeque<Thread> pizzaOrders = new LinkedBlockingDeque(2);
+    LinkedBlockingDeque<Order> orders = new LinkedBlockingDeque<>();
+    final long START_TIME;
 
-    void order(String pizzaName) throws InterruptedException {
-        long now = System.currentTimeMillis();
-        if (now - start > 5000) {
-            System.out.println("Too late <5000ms");
-        } else {
-            Thread wagon = new Thread() {
-                public void run() {
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
+    Pizzeria(){
+        START_TIME = System.currentTimeMillis();
+        new PizzaCar().start();
+        new PizzaCar().start();
+    }
+    class PizzaCar extends Thread{
+        public void run(){
+            while(System.currentTimeMillis() -  START_TIME < 5000){
+                Order order;
+                try{
+                    order = orders.poll(10, TimeUnit.MILLISECONDS);
+                }catch(InterruptedException e){break;}
+                if (order != null){
+                    if (System.currentTimeMillis() + 500 - order.orderTime <= 750){
+                        //принять заказ
+                        try{
+                            Thread.sleep(500);
+                        }catch(InterruptedException e){break;}
+                        System.out.println(order.pizzaName + " is delivered");
                     }
-                    System.out.println(pizzaName + " is delivered");
-                    pizzaOrders.poll();
+                    else{
+                        //отменить заказ
+                        System.out.println(order.pizzaName + " is NOT delivered");
+                    }
                 }
-            };
-            boolean added = pizzaOrders.offer(wagon, 250, TimeUnit.MILLISECONDS);
-
-            if (added) wagon.start();
-            else System.out.println(pizzaName + " is NOT delivered");
+            }
+        }
+    }
+    void order(String pizzaName){
+        try{
+            orders.put(new Order(pizzaName, System.currentTimeMillis()));
+        }catch(InterruptedException e){
+            return;
         }
     }
 }
